e1bdadfab4d4ff7af9b7a26f8979c317e9b51899
Jim Kroon <jim.kroon@deptagency.com>
2019-08-23 20:21:49 +0200

    Fix component

--- xboxone/run.sh
+++ xboxone/run.sh
@@ -2,7 +2,7 @@
 
 # Copy over the xbox component
 mkdir -p /config/custom_components/media_player
-cp -f xboxone.py /config/custom_components/media_player/xboxone.py
+cp -f xboxone.py /config/custom_components/xboxone/media_player.py
 
 touch /config/.xbox-token.json
 
