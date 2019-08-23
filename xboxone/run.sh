ec3e755efdfa01bb98d7688c77eabbc09378a3c4
Jim Kroon <jim.kroon@deptagency.com>
2019-08-23 21:14:39 +0200

    Update modules

--- xboxone/run.sh
+++ xboxone/run.sh
@@ -2,7 +2,9 @@
 
 # Copy over the xbox component
 mkdir -p /config/custom_components/xboxone
-cp -f xboxone.py /config/custom_components/xboxone/media_player.py
+cp -f /xboxone.py /config/custom_components/xboxone/media_player.py
+cp -f /manifest.json /config/custom_components/xboxone/manifest.json
+touch /config/custom_components/xboxone/__init__.py
 
 touch /config/.xbox-token.json
 
